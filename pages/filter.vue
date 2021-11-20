3a08bb5ce122b898e9d86b35de41ef24a4b619aa
Lukas Mateffy <luke@mateffy.me>
2021-11-20 10:56:28 +0100

    Frontend start

---
+++ pages/filter.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card elevation="4">ello</v-card>
+        <h1 class="mb-2">Willkommen im Code&Create Co-working space!</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
