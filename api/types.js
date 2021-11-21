2e82a21de80a4cfc1087d82cd7586575b349a6ea
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2021-11-21 09:12:31 +0100

    feat: separated types and mail system

---
+++ api/types.js
@@ -0,0 +1,37 @@
+/**
+ * @typedef User
+ * @type {object}
+ * @property {string} mail
+ * @property {string} password
+ */
+
+/**
+ * @typedef Room
+ * @type {object}
+ * @property {number} id
+ * @property {Table[]} tables
+ */
+
+/**
+ * @typedef Booking
+ * @type {object}
+ * @property {number} id
+ * @property {number} tableId
+ * @property {number} roomId
+ * @property {number} from
+ * @property {number} to
+ * @property {string} by
+ * @property {boolean} checkedIn
+ */
+
+/**
+ * @typedef Table
+ * @type {object}
+ * @property {number} id
+ * @property {boolean} bookable
+ * @property {boolean} disabled
+ * @property {Booking[]} booked
+ * @property {string[]} properties
+ */
+
+module.exports = {  };
